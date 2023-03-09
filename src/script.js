1e996908c133aaa3cd4cface24ca6e1ef653dfb4
TheodoraPrayogo <113406918+TheodoraPrayogo@users.noreply.github.com>
2023-03-09 17:57:00 -0600

    Integrate Api for Tem, city, country

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
-function displayTemperature (response) {
-    console.log (response);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = response.data.country;
 }
 
-let apiKey = "37e74cb00c288f7eo2938f4t06eaf3b2";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=houston&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "37e74cb00c288f7eo2938f4t06eaf3b2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Houston&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
