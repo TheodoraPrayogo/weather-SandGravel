2ecca213fff567580039b4827e558ab3b91efa73
TheodoraPrayogo <113406918+TheodoraPrayogo@users.noreply.github.com>
2023-03-09 22:39:35 -0600

    Add Display Temperature in JS

--- src/script.js
+++ src/script.js
@@ -1,15 +1,29 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-
-  let cityElement = document.querySelector("#city");
-  console.log(response.data);
+function displayTemperature (response) {
  
-}
 
-function search(city) {
-  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+  let temperatureElement= document.querySelector ("#temperature");
+  temperatureElement.innerHTML= Math.round(response.data.temperature.current);
+
+  let cityElement=document.querySelector ("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement=document.querySelector ("#description");
+  descriptionElement.innerHTML=response.data.condition.description ;
+
+  let humidityElement=document.querySelector("#humidity");
+  humidityElement.innerHTML=response.data.temperature.humidity;
+
+  let windElement=document.querySelector("#wind");
+  windElement.innerHTML=response.data.wind.speed;
+
+}
+  
+
+
+
+  let apiKey = "37e74cb00c288f7eo2938f4t06eaf3b2";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Houston&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
-}
+
+
