24112617511e7ad7565925ec3b5d1495ba35a4d0
TheodoraPrayogo <113406918+TheodoraPrayogo@users.noreply.github.com>
2023-03-09 18:00:44 -0600

    Change API and retest the integration

--- src/script.js
+++ src/script.js
@@ -3,14 +3,12 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
-
-  let countryElement = document.querySelector("#country");
-  countryElement.innerHTML = response.data.country;
+  console.log(response.data);
+ 
 }
 
 function search(city) {
-  let apiKey = "37e74cb00c288f7eo2938f4t06eaf3b2";
+  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Houston&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
